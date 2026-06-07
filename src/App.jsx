6004654baa67a29aa8e58234b8ecf10a1b0e02b4
Muhammad Rizky Hidayah Akbar <mhidayahakbar@gmail.com>
2026-06-07 20:02:47 +0700

    PR 29 bagian 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 // import PR29_2 from './PR29_2.jsx';
 // import PR29_3 from './PR29_3.jsx';
 // import PR29_4 from './PR29_4.jsx';
-import PR29_5 from './PR29_5.jsx';
+// import PR29_5 from './PR29_5.jsx';
+import PR29_6 from './PR29_6.jsx';
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       {/* <PR29_2 /> */}
       {/* <PR29_3 /> */}
       {/* <PR29_4 /> */}
-      <PR29_5 />
+      {/* <PR29_5 /> */}
+      <PR29_6 />
     </div>
   );
 }
---
+++ src/PR29_6.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+
+function withLogger(WrappedComponent) {
+  return function WithLogger(props) {
+    useEffect(() => {
+      console.log("Component has mounted");
+      
+      return () => {
+        console.log("Component will unmount!");
+      };
+    }, []);
+
+    return <WrappedComponent {...props} />;
+  };
+}
+
+function MyComponent() {
+  return <div>Belajar HOC!</div>;
+}
+
+const MyComponentWithLogger = withLogger(MyComponent);
+
+export default MyComponentWithLogger;
